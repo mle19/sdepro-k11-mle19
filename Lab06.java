87b4418e6e7127f37c59e2198ee77cfef62f2681
myle19 <110147086+mle19@users.noreply.github.com>
2022-09-30 19:20:14 +0700

    Add files via upload

---
+++ Lab06.java
@@ -0,0 +1,46 @@
+package Lab06;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lab06 {
+    public static void main(String[] args) {
+        //Lab 6.1
+        String currenttime = "2hrs and 5 minutes";
+        System.out.println(currenttime);
+        String currenttimehr = currenttime.replaceAll("[^2]", "");
+        String currenttimemt = currenttime.replaceAll("[^5]", "");
+        System.out.println("Total is "+ (Integer.parseInt(currenttimemt) + Integer.parseInt(currenttimehr)*60) + " minutes");
+        //Lab 6.2
+        Inputpass3times();
+        //Lab 6.3
+        String minute = "100 minutes";
+        System.out.println(minute);
+        String[] minutes = minute.split(" minutes");
+        System.out.println(Arrays.toString(minutes));
+        //Lab 6.4
+        
+
+
+
+
+    }
+    private static void Inputpass3times(){
+        System.out.println("The password should be like: password123. Pleas enter your password for three time");
+        int validpass = 0;
+        int invalidpass = 0;
+        String[] InArr = new String[3];
+        for(int i = 0; i <3; i ++){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Turn "+ (i+1) + " :");
+             InArr[i] = scanner.toString();
+            if(InArr[i].equals("password123")){
+                validpass ++;
+            }else invalidpass ++;
+        }
+        if(validpass == 3){
+            System.out.println("Correct Password");
+        }else System.out.println("Invalid Password");
+    }
+
+}

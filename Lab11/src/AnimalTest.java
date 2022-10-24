4df627e7ec374adbfe929494f49002b3e1ce6731
Le Thi Tra My <lethitramy@Les-MacBook-Pro.local>
2022-10-24 17:19:10 +0700

    Lab11

--- Lab11/src/AnimalTest.java
+++ Lab11/src/AnimalTest.java
@@ -5,14 +5,14 @@ import java.util.List;
 public class AnimalTest {
     public static void main(String[] args) {
         Animal dog = new Dog();
-        dog.setName(dog.getName());
-        dog.setSpeed(dog.getSpeed());
+        dog.getName();
+        dog.getSpeed();
         Animal cat = new Cat();
-        cat.setName(cat.getName());
-        cat.setSpeed(cat.getSpeed());
+        cat.getName();
+        cat.getSpeed();
         Animal tiger = new Tiger();
-        tiger.setName(tiger.getName());
-        tiger.setSpeed(tiger.getSpeed());
+        tiger.getName();
+        tiger.getSpeed();
         List<Animal> anm = Arrays.asList(dog,cat,tiger);
         System.out.println(anm);
         String winnername = new Animalcontroller().Findmax(anm);
